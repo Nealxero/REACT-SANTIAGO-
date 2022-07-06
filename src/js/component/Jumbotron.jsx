7ff41c6d0adba5236ce60baa08078b13b31127ff
Waro <waro.brambilla@gmail.com>
2022-07-06 17:40:17 +0000

    Borders to jumbotron

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Jumbotron() {
   return (
         <>
-           <div class="bg-light p-5 rounded-lg m-3">
+           <div class="bg-light p-5 rounded-lg m-3 block-example border border-info rounded mb-0">
                 <h1 class="display-4">What is a Jumbotron?</h1>
                     <p class="lead">This is a simple element, a component for calling extra attention to featured content or information.</p>
                 <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
