5c4b7ce8eb8d26acb49524083b91f3759c3472cc
SimoneHell <simone.hall.speaks@gmail.com>
2022-07-06 18:18:30 +0000

    add styling for dynamic cards. Thanks Santiago

--- src/js/component/cardcomponent.jsx
+++ src/js/component/cardcomponent.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
+const CardStyle = {
+  margin: "5px",
+}
+
 const CardComponent = (props) =>
-{return <div className="card col-3">
+{return <div className="card" style={CardStyle}>
   <img src="https://via.placeholder.com/500x325" className="card-img-top" alt="..."/>
 	<div className="card-body">
     <h5 className="card-title">{props.cardTitle}</h5>
